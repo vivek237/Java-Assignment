04c3c3e4daaa2c59150de44ed7260790971268a6
vivek237 <73047756+vivek237@users.noreply.github.com>
2021-09-11 15:53:33 +0530

    Add files via upload

---
+++ Thread/ThreadsDemoByRun.java
@@ -0,0 +1,39 @@
+package practice;
+
+class ThreadsRunDemos extends Thread {
+	 
+    public void run() { // imp method given by thread class java
+ 
+        //logic part
+        try {
+            System.out.println("list of threads running " + Thread.currentThread());
+        } catch (Exception e) {
+ 
+            System.out.println("exceotion caught" + e);
+ 
+        }
+ 
+    }
+ 
+}
+ 
+public class ThreadsDemoByRun {
+ 
+    public static void main(String ar[]) {
+ 
+        int numberOfThreads = 2;
+ 
+        for (int i = 0; i < numberOfThreads; i++) {
+ 
+        	ThreadsRunDemos threadsDemos = new ThreadsRunDemos();//created the object of a class
+ 
+            threadsDemos.start();// life cycle of threads start() call the run method
+            threadsDemos.setName("abhinav thread");
+            threadsDemos.setPriority(8);
+ 
+        }
+ 
+    }
+ 
+
+}
---
+++ Thread/ThreadsSleepDemo.java
@@ -0,0 +1,38 @@
+package practice;
+
+class ThreadssleepDemos extends Thread {
+		 
+	    public void run() { // imp method given by thread class java
+	 
+	        // logic part
+	        try {
+	            for (int i = 1; i < 5; i++) {
+	                Thread.sleep(5000);
+	                System.out.println(i);
+	            }
+	 
+	        } catch (Exception e) {
+	 
+	            System.out.println("exception caught" + e);
+	 
+	        }
+	 
+	    }
+	 
+	}
+	 
+	public class ThreadsSleepDemo {
+	 
+	    public static void main(String ar[]) {
+	 
+	        ThreadssleepDemos threadsDemos = new ThreadssleepDemos();// created the object of a class
+	 
+	    
+
+	        threadsDemos.start();
+	 
+
+	    }
+	 
+
+}
