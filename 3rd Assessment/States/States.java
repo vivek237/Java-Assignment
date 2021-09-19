023b51abf7c2c8c2e8ff1d92bcdcac3e83b84ca2
vivek237 <73047756+vivek237@users.noreply.github.com>
2021-09-20 01:36:04 +0530

    Add files via upload

---
+++ 3rd Assessment/States/States.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class States {
+	
+	public static void main(String args[])throws IOException {
+		
+		FileReader fileReader=new FileReader("D:\\States.text");
+		BufferedReader bufferedReader=new BufferedReader(fileReader);
+		Set<String> stateSet=new LinkedHashSet<String>();
+		
+		try {
+			String reader=bufferedReader.readLine();
+		
+			while(reader!=null) {
+				stateSet.add(reader);
+				reader=bufferedReader.readLine();
+			}
+			System.out.println("Total number of states : "+stateSet.size());
+			System.out.println("Remove delhi : "+stateSet.remove("Delhi"));
+			System.out.println("After removing the Delhi : "+stateSet);
+			System.out.print("State which starts with k : ");
+			for(String state:  stateSet) {
+				if(state.charAt(0)=='K' || state.charAt(0)=='k') {
+					System.out.print(state+"  ");
+				}
+			}
+			//Sorting 
+			List<String> stateList=new ArrayList<String>(stateSet);
+			Collections.sort(stateList);
+			Set<String> statesort=new LinkedHashSet<>(stateList);
+			System.out.println("\nStates in sorted order : "+statesort);
+			
+			
+			
+		}
+		finally {
+			bufferedReader.close();
+		}
+				
+	}
+
+}
