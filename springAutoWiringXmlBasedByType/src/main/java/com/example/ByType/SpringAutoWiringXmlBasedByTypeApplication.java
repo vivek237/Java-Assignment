ee4ed6bcbc74d2e60ffc940e343c1e5712aff03a
vivek237 <73047756+vivek237@users.noreply.github.com>
2021-09-29 18:17:30 +0530

    Add files via upload

---
+++ springAutoWiringXmlBasedByType/src/main/java/com/example/ByType/SpringAutoWiringXmlBasedByTypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ByType;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoWiringXmlBasedByTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAutoWiringXmlBasedByTypeApplication.class, args);
+	}
+
+}
