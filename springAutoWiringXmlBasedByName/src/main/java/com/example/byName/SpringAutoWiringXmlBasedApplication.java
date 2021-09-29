ddc7891770f497ca01271f127509ba301952da43
vivek237 <73047756+vivek237@users.noreply.github.com>
2021-09-29 18:15:42 +0530

    Add files via upload

---
+++ springAutoWiringXmlBasedByName/src/main/java/com/example/byName/SpringAutoWiringXmlBasedApplication.java
@@ -0,0 +1,13 @@
+package com.example.byName;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAutoWiringXmlBasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAutoWiringXmlBasedApplication.class, args);
+	}
+
+}
---
+++ springAutoWiringXmlBasedByName/src/main/java/com/example/byName/Test.java
@@ -0,0 +1,12 @@
+package com.example.byName;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("application.xml");
+		Employee emp=context.getBean("emp",Employee.class);
+		System.out.println(emp);
+	}
+}
