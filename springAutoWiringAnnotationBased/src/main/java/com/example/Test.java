77e44d14a59adfe500866813046af03f72972c19
vivek237 <73047756+vivek237@users.noreply.github.com>
2021-09-29 18:13:24 +0530

    Add files via upload

---
+++ springAutoWiringAnnotationBased/src/main/java/com/example/Test.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("application.xml");
+		Employee emp=context.getBean("emp",Employee.class);
+		System.out.println(emp);
+		
+	}
+}
