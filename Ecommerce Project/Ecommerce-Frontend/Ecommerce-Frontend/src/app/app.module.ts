71f2eac2c8c2f3123c6a8168364deccae0c8e156
vivek237 <73047756+vivek237@users.noreply.github.com>
2021-10-18 14:47:55 +0530

    Add files via upload

---
+++ Ecommerce Project/Ecommerce-Frontend/Ecommerce-Frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    CreateUserComponent,
+    UpdateUserComponent,
+    UserDetailsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
